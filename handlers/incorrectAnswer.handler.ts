bc85923a112b206829ef931912daa8999a9daa10
onetyten <labaekaabdulrazaq@gmail.com>
2026-02-16 14:59:44 +0100

    remove incorrectAnswerHandler

--- handlers/incorrectAnswer.handler.ts
+++
@@ -1,6 +0,0 @@
-import { Socket } from "socket.io";
-import { ChatType } from "../types/type";
-
-export async function WrongAnswerMessage(socket: Socket, { user,color,message, timeStamp }: ChatType) {
- return socket.to(user.room_id).emit("questionAnsweredWrong", { user, message, timeStamp,color });
-}
