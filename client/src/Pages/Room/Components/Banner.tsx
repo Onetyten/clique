d462dcb14db72328146297138c571cb6b14a6916
onetyten <labaekaabdulrazaq@gmail.com>
2026-02-17 16:50:16 +0100

    ticking clock

--- client/src/Pages/Room/Components/Banner.tsx
+++ client/src/Pages/Room/Components/Banner.tsx
@@ -1,5 +1,5 @@
-import gsap from "gsap"
-import { useEffect } from "react"
+// import gsap from "gsap"
+import { useRef } from "react"
 
 interface propType{
   bannerMessage:string
@@ -8,15 +8,25 @@ interface propType{
 
 export default function Banner({bannerMessage}:propType) {
 
-  useEffect(()=>{
-      const tl = gsap.timeline()
-      tl.from("#banner-bg",{width:0,ease:"none", duration:1}).from("#banner-text",{opacity:0,duration:0.2,delay:0.1})  
-  },[])
+  const bgRef = useRef<HTMLDivElement | null>(null);
+  const textRef = useRef<HTMLParagraphElement | null>(null);
+
+  // useEffect(() => {
+  //   const tl = gsap.timeline();
+
+  //   tl.from(bgRef.current, { scaleX: 0, transformOrigin: "left center", duration: 0.4, ease: "power2.out",})
+  //   .from(textRef.current, { opacity: 0, duration: 0.2 });
+
+  //   return () => {
+  //     tl.kill();
+  //   };
+  // }, []);
+
 
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-background/50 backdrop-blur-sm z-40">
-        <div id="banner-bg" className="p-12 bg-accent-blue w-full text-sm text-white rounded-sm flex justify-center items-center">
-            <p id="banner-text" className="text-3xl sm:text-4xl font-bold">{bannerMessage}</p>
+        <div ref={bgRef} className="p-12 bg-accent-blue w-full opacity-100 text-sm text-white rounded-sm flex justify-center items-center">
+            <p ref={textRef} className="text-3xl opacity-100 sm:text-4xl font-bold">{bannerMessage}</p>
         </div>
     </div>
   )
--- client/src/hooks/useRoomSocketListeners.ts
+++ client/src/hooks/useRoomSocketListeners.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { socket } from "../util/socket"
 import type { RootState } from "../util/store"
 import { useDispatch, useSelector } from "react-redux"
@@ -11,6 +11,9 @@ import { clearSession, setSession } from "../store/sessionSlice"
 import { setUser } from "../store/userSlice"
 import store from "../util/store"
 import joinAudio from "/public/Audio/join.mp3"
+import clockTick from "/public/Audio/clock-ticking.mp3"
+import clockTickUrgent from "/public/Audio/clock-ticking-urgent.mp3"
+import Bell from "/public/Audio/school_bell.mp3"
 import pointObtainedAudio from "/public/Audio/points-obtained.mp3"
 import confetti from "canvas-confetti";
 
@@ -21,7 +24,7 @@ import confetti from "canvas-confetti";
 
 
 export function victoryConfetti() {
-  const duration = 5 * 1000;
+  const duration = 4 * 1000;
   const end = Date.now() + duration;
 
   const colors = ["#5865F2", "#57F287"];
@@ -60,6 +63,9 @@ export default function useRoomSocketListeners(){
     const [timeLeft,setTimeleft] = useState<number>(60)
     const [bannerMessage,setBannerMessage] = useState(`${roundCount}`)
     const [showQuestionForm,setShowQuestionForm] = useState(false)
+    const clockRef = useRef<HTMLAudioElement | null>(null);
+    const urgentRef = useRef<HTMLAudioElement | null>(null);
+    const bellRef = useRef<HTMLAudioElement | null>(null);
 
 
     async function sessionCleanUp() {
@@ -85,6 +91,31 @@ export default function useRoomSocketListeners(){
             console.log('error fetching members',error)
         }
     }
+    useEffect(() => {
+        clockRef.current = new Audio(clockTick);
+        urgentRef.current = new Audio(clockTickUrgent);
+        bellRef.current = new Audio(Bell);
+
+        if (clockRef.current) {
+            clockRef.current.loop = true;
+            clockRef.current.volume = 0.4;
+        }
+
+        if (urgentRef.current) {
+            urgentRef.current.loop = true;
+            urgentRef.current.volume = 0.6;
+        }
+
+        if (bellRef.current) {
+            bellRef.current.volume = 0.8;
+        }
+
+        return () => {
+            clockRef.current?.pause();
+            urgentRef.current?.pause();
+        };
+    }, []);
+
 
     useEffect(()=>{
         getFriendList()
@@ -93,22 +124,50 @@ export default function useRoomSocketListeners(){
 
     useEffect(() => {
         if (!session) return;
+
+        clockRef.current?.play();
+
         const interval = setInterval(() => {
             setTimeleft(prev => {
-                if (prev <= 1) {
-                    clearInterval(interval);
-                    dispatch(clearSession());
-                    setBannerMessage("Time's Up");
-                    setShowBanner(true);
-                    return 0;
-                }
-                return prev - 1;
+            if (prev <= 1) {
+                clearInterval(interval);
+                clockRef.current?.pause();
+                urgentRef.current?.pause();
+                clockRef.current!.currentTime = 0;
+                urgentRef.current!.currentTime = 0;
+                bellRef.current?.play();
+                dispatch(clearSession());
+                setBannerMessage("Time's Up");
+                setShowBanner(true);
+                return 0;
+            }
+
+            return prev - 1;
             });
         }, 1000);
 
-        return () => clearInterval(interval);
+        return () => {
+            clearInterval(interval);
+            clockRef.current?.pause();
+            urgentRef.current?.pause();
+        };
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [session]);
+
+    const hasSwitchedToUrgent = useRef(false);
+
+    useEffect(() => {
+        if (!session) return;
+
+        if (timeLeft <= 10 && !hasSwitchedToUrgent.current) {
+            hasSwitchedToUrgent.current = true;
+
+            clockRef.current?.pause();
+            urgentRef.current?.play();
+        }
+    }, [timeLeft, session]);
+
+
 
     useEffect(() => {
         if (!showBanner) return;
@@ -176,6 +235,7 @@ export default function useRoomSocketListeners(){
             sessionCleanUp()
             dispatch(clearSession())
             getFriendList()
+            toast.info(data.adminMessage)
             setBannerMessage(data.message)
             setShowBanner(true)
             playSound(pointObtainedAudio,1)
